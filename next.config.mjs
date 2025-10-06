523093786df959e8643f57316b359be56b472a7d
SurendranGithub <prasannarajan2107@gmail.com>
2025-10-06 22:18:47 +0530

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: ['**/C:/Users/Surendran/Cookies/**'], // ignore the protected path
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
